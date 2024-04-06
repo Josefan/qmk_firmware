85bbcb13b775200e725ad5e1a46f9b1be38a6c57
Josef <josefandersson@outlook.com>
2024-04-06 12:03:19 +0200

    Disable unused functions

--- keyboards/crkbd/keymaps/josefan/rules.mk
+++ keyboards/crkbd/keymaps/josefan/rules.mk
@@ -1,8 +1,12 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+MOUSEKEY_ENABLE    = no    # Mouse keys
+RGBLIGHT_ENABLE    = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE        = no
+OLED_DRIVER        = SSD1306
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE   = no 
+MAGIC_ENABLE       = no
+LTO_ENABLE         = yes
+UNICODEMAP_ENABLE  = yes
 
 # Bootloader selection
 BOOTLOADER = atmel-dfu
