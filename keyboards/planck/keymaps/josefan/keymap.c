32b85feebc51617c672166ade731151ecfbcb3e2
Josef <josefandersson@outlook.com>
2024-10-15 16:13:39 +0200

    Adding modtap to ctrl/esc

--- keyboards/planck/keymaps/josefan/keymap.c
+++ keyboards/planck/keymaps/josefan/keymap.c
@@ -51,18 +51,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  Å   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |Ctl/ESC|  A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Esc | LAlt  | GUI  | Space/Lower | Bksp/Raise  |ENT/GUI|Gaming| Left | Right|
+ * |      | Esc | LAlt  | GUI  | Space/Lower | Bksp/Raise  | ENT  |Gaming| Left | Right|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,      KC_U,    KC_I,    KC_O,         KC_P,       SE_ARNG,
-    KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,      KC_J,    KC_K,    KC_L,         KC_SCLN,    KC_QUOT,
-    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,      KC_M,    KC_COMM, KC_DOT,       KC_SLSH,    KC_DEL,
-    TG(_NUMPAD), KC_ESC,  KC_LALT, KC_LGUI, _______, LT(_LOWER, KC_SPC),_______, LT(_RAISE, KC_BSPC),KC_ENT,  TG(_GAMING),  KC_LEFT,    KC_RGHT
+    KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,      KC_U,    KC_I,    KC_O,         KC_P,       SE_ARNG,
+    LCTL_T(KC_ESC),KC_A   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,      KC_J,    KC_K,    KC_L,         KC_SCLN,    KC_QUOT,
+    KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,      KC_M,    KC_COMM, KC_DOT,       KC_SLSH,    KC_DEL,
+    TG(_NUMPAD),   KC_ESC,  KC_LALT, KC_LGUI, _______, LT(_LOWER, KC_SPC),_______, LT(_RAISE, KC_BSPC),KC_ENT,  TG(_GAMING),  KC_LEFT,    KC_RGHT
 ),
 
 /* Raise
